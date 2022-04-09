eaa0cac154a9299b3ab30635877ee52f0bf5fb82
Eric Chang <82834722+echang505@users.noreply.github.com>
2022-04-09 15:47:55 -0400

    Updated main.py
    allows script to upload data into excel files

--- Scraper/main.py
+++ Scraper/main.py
@@ -2,8 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 
+
+
 import time
 import sys
+import csv
 
 # South, 251, Diner
 dinerSelectionXPaths = ['/html/body/center/table[2]/tbody/tr/td[1]/table/tbody/tr[1]/td/table/tbody/tr[1]/td[1]/table/tbody/tr/td[2]/table/tbody/tr[1]/td/span/a',
@@ -12,6 +15,8 @@ dinerSelectionXPaths = ['/html/body/center/table[2]/tbody/tr/td[1]/table/tbody/t
 # Breakfast, Lunch, Dinner
 mealXPaths = ['/html/body/table/tbody/tr[2]/td[2]/table[2]/tbody/tr/td[1]/table/tbody/tr[1]/td/table/tbody/tr/td[2]/a',
               '/html/body/table/tbody/tr[2]/td[2]/table[2]/tbody/tr/td[2]/table/tbody/tr[1]/td/table/tbody/tr/td[2]/a', '/html/body/table/tbody/tr[2]/td[2]/table[2]/tbody/tr/td[3]/table/tbody/tr[1]/td/table/tbody/tr/td[2]/a']
+
+mealTypeXPaths = ['/html/body/table/tbody/tr[2]/td[2]/table[2]/tbody/tr/td[1]/table/tbody/tr[1]/td/table/tbody/tr/td[1]/div', '/html/body/table/tbody/tr[2]/td[2]/table[2]/tbody/tr/td[2]/table/tbody/tr[1]/td/table/tbody/tr/td[1]/div', '/html/body/table/tbody/tr[2]/td[2]/table[2]/tbody/tr/td[3]/table/tbody/tr[1]/td/table/tbody/tr/td[1]/div' ]
 
 
 southItems = {}
@@ -31,11 +36,15 @@ for iteration, dinerLink in enumerate(dinerSelectionXPaths):
     # click on nutrition information for each meal - breakfast, lunch, dinner
     for number, mealLink in enumerate(mealXPaths):
         driver.implicitly_wait(3)
+        
         try:
+            mealType = driver.find_element_by_xpath(mealTypeXPaths[number]).text 
             element = driver.find_element_by_xpath(mealLink)
             element.click()
         except NoSuchElementException:
             break
+
+        #find out which breakfast/lunch/dinner
 
         # put one into each of the table elements
         tableXPath = '/html/body/table/tbody/tr[2]/td[2]/div[2]/table[1]/tbody'
@@ -68,12 +77,15 @@ for iteration, dinerLink in enumerate(dinerSelectionXPaths):
                 # name
                 id = tableElements[tabNum].find_element_by_class_name(
                     'nutrptnames').find_element_by_tag_name('a').text
-                # porperties: [portion, cal, prot, carb, transF, vitC,  Sod]
+                # porperties: [mealtype, portion, cal, prot, carb, transF, vitC,  Sod]
                 properties = []
+                properties.append(mealType) #breakfast/lunch/dinner
                 properties.append(
                     tableElements[tabNum].find_element_by_class_name('nutrptportions').text)
+                #makes a list of all divs containing nutrition information
                 nutrition = tableElements[tabNum].find_elements_by_class_name(
                     'nutrptvalues')
+                #covert divs into their text values and appends to properties
                 for nutritionType in nutrition:
                     properties.append(nutritionType.text)
                 if iteration == 0:
@@ -110,3 +122,54 @@ def printDict(x):
 printDict(southItems)
 printDict(north251Items)
 printDict(dinerItems)
+
+
+
+
+def writeToCsv(dict_data, name):
+    # ['1 oz', '22.964', '0.743', '5.270', '0.000', '2.410', '60.669']
+
+    csv_columns = ['Food Name', 'Meal', 'Portion', 'Calories', 'Protein', 'Carbs', 'TransFat', 'Vitamin C',  'Sodium']
+    csv_file = name
+
+    try:
+        with open(csv_file, "w") as outfile:
+            writerfile = csv.writer(outfile)
+            writerfile.writerow(csv_columns)
+            for key, value in dict_data.items():
+                writerfile.writerow([key, value[0], value[1], value[2], value[3], value[4], value[5], value[6], value[7]])
+                
+            outfile.close()
+
+    except IOError:
+        print("I/O error")
+
+    """  try:
+        with open(csv_file, "w") as outfile:
+            writerfile = csv.writer(outfile)
+            writerfile.writerow(dict_data.keys())
+            writerfile.writerows(zip(*dict_data.values()))
+            outfile.close()
+
+    except IOError:
+        print("I/O error")
+    """
+    
+
+
+"""     try:
+        with open(csv_file, 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            for key, value in dict_data.items():
+                writer.writerow([key, value])
+
+            csvfile.close()
+    except IOError:
+        print("I/O error") """
+
+
+
+
+writeToCsv(southItems, 'southDiner.csv')
+writeToCsv(north251Items, 'north251Diner.csv')
+writeToCsv(dinerItems, 'Diner.csv')
